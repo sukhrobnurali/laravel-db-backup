a96cb758342589cae2c5145c55f5d859ce5a2ae1
sukhrobnuraliev <sukhrobnuralievv@gmail.com>
2024-06-10 22:21:00 +0500

    Created Service Provider

---
+++ src/providers/DatabaseBackupServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace YourVendor\LaravelDatabaseBackup\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class DatabaseBackupServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any package services.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        if ($this->app->runningInConsole()) {
+            // Publish the configuration file.
+            $this->publishes([
+                __DIR__.'/../../config/backup.php' => config_path('backup.php'),
+            ], 'config');
+
+
+        }
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register(): void
+    {
+        // Merge package configuration with the application's configuration.
+        $this->mergeConfigFrom(
+            __DIR__.'/../../config/backup.php',
+            'backup'
+        );
+    }
+}
